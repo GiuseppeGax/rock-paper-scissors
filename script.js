4fbc272e0c77d1d884fa375cff139defc1a118eb
GiuseppeGax <169217745+GiuseppeGax@users.noreply.github.com>
2024-11-13 15:04:25 -0600

    done-all-done

--- script.js
+++ script.js
@@ -3,70 +3,72 @@ let humanscore = 0;
 let computerscore = 0;
 
 
-console.log(PlayGame());
-
-
-function PlayGame(){
-    for (let i = 0; i < 5;i++){
-        PlayRound();
-    }
-    if(humanscore > computerscore){
-        console.log("you win!")
-    }
-    else{
-        console.log("you lose")
-    }
-    
-}
-
-
-function PlayRound(){
-    let humanchoice = GetHumanChoice();
+function PlayRound(humanchoice){
     let computerchoice = GetComputerChoice();
 
     if(humanchoice == computerchoice){
-        printscore();
-        console.log("tie")
+        UpdateScore();
+        document.getElementById("pr").textContent = "you tied this round!";
         return;
     }
     else if(humanchoice == "rock"){
         if(computerchoice == "paper"){
             computerscore++;
+            document.getElementById("pr").textContent = "you lose this round"
         }
         else{
             humanscore++;
+            document.getElementById("pr").textContent = "you win this round!";
         }
     }
     else if(humanchoice == "paper"){
         if(computerchoice == "rock"){
             humanscore++;
+            document.getElementById("pr").textContent = "you win this round!";
         }
         else{
             computerscore++;
+            document.getElementById("pr").textContent = "you lose this round"
         }
     }
     else{
         if(computerchoice == "rock"){
             computerscore++;
+            document.getElementById("pr").textContent = "you lose this round!";
             printscore()
         }
         else{
             humanscore++;
+            document.getElementById("pr").textContent = "you win this round!";
         }
     }
-    printscore();
-    if(humanscore > computerscore){
-        console.log("you win this round!")
+    UpdateScore();
+    console.log(humanscore)
+    console.log(computerscore)
+    setTimeout(Decide, 250)
+}
+    function Decide(){
+    if(humanscore == 5){
+        alert("You WON!");
+        humanscore = 0;
+        computerscore = 0;
+        UpdateScore();
+        document.getElementById("pr").textContent = "pr";
     }
-    else{
-        console.log("you lose this round")
+    else if(computerscore == 5){
+        alert("You Lost");
+        humanscore = 0;
+        computerscore = 0;
+        UpdateScore();
+        document.getElementById("pr").textContent = "pr";
     }
 }
 
-function printscore(){
-    console.log("Your score: " + humanscore);
-    console.log("Computer score: " + computerscore);
+function UpdateScore(){
+    document.getElementById("hs").textContent = "Human Score: " + humanscore;
+    document.getElementById("cs").textContent = "Computer Score: " + computerscore;
 }
+
 
 function GetComputerChoice(){
     let b =(Math.floor(Math.random()*3));
@@ -82,14 +84,25 @@ function GetComputerChoice(){
     return b;
 }
 
-function GetHumanChoice(){
-    let a;
-    while(a != "rock" && a != "paper" && a!= "scissors"){
-        a = prompt("rock, paper, or scissors");
-        a = a.toLowerCase();
-        if (a != "rock" && a != "paper" && a!= "scissors"){
-            alert( a +" is an invalid response");
-        }
-    }
-    return a;
-}
+
+    const rock = document.querySelector("#rock");
+    const paper = document.querySelector("#paper");
+    const scissors =document.querySelector("#scissors");
+
+
+    const hs = document.querySelector("#hs");
+    const cs = document.querySelector("#cs");
+
+    
+
+    rock.onclick = () => PlayRound("rock");
+    paper.onclick = () => PlayRound("paper");
+    scissors.onclick = () => PlayRound("scissors");
+    
+    // = (PlayRound){}
+
+/*
+so we have out buttons
+when we click o nbutton it will return the value of the button which is the textof the button
+we will play the game (whcih is computer generate etc until one person reaches 5 wins)
+*/
